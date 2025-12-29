847f2a034361a1492a080dcf1314f4c5a2d47849
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2025-12-29 09:34:25 -0300

    add copilot to nvim

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,       -- show suggestions automatically
+      hide_during_completion = true,
+      keymap = {
+        accept = "<C-l>",
+        next = "<M-]>",          -- next suggestion
+        prev = "<M-[>",          -- previous suggestion
+        dismiss = "<C-e>",       -- ignore suggestion
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
