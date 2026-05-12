f94724fb100ac90b279a6cfa61974a3ee8c86198
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-05-12 15:58:27 -0300

    configurate default formatter prittier

--- nvim/lua/plugins/prettier.lua
+++ nvim/lua/plugins/prettier.lua
@@ -3,12 +3,19 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        html = { "prettier" },
+        css = { "prettier" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
         json = { "prettier" },
         jsonc = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { "--print-width", "75" },
+        },
       },
     },
   },
