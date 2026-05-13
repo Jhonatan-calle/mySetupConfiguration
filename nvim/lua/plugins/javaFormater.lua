2b71f4d8d866850cdd0c2a1c57acad60fdf4059d
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-05-13 16:06:46 -0300

    add java formater

---
+++ nvim/lua/plugins/javaFormater.lua
@@ -0,0 +1,17 @@
+#para esto hay que instalar google-java-format en mason
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+
+      formatters_by_ft = {
+        java = { "google-java-format" },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { "--print-width", "75" },
+        },
+      },
+    },
+  },
+}
