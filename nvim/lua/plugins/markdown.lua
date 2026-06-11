1edce37f3c559fa83ac8b2b1f7d9bc6cd1d19c99
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-06-11 12:04:08 -0300

    change browser to render md

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,17 @@
 return {
   "iamcco/markdown-preview.nvim",
   ft = { "markdown" },
+  init = function()
+    vim.cmd([[
+      function! OpenPreview(url) abort
+        call system('qutebrowser ' . a:url . ' &')
+      endfunction
+    ]])
+    vim.g.mkdp_browserfunc = "OpenPreview"
+  end,
+  keys = {
+    { "<leader>mp", ":MarkdownPreviewToggle<CR>", desc = "Toggle markdown preview" },
+  },
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
