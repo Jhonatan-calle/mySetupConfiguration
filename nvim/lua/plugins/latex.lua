8e4f8374d19fda9762499339dac2691d1bb61a74
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-06-12 12:45:47 -0300

    outofollew cursos latex

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -4,6 +4,8 @@ return {
   init = function()
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_view_forward_search_on_start = true
+    vim.g.vimtex_tracking = false
+
     vim.g.vimtex_compiler_latexmk = {
       build_dir = "",
       callback = 1,
@@ -16,5 +18,26 @@ return {
         "-interaction=nonstopmode",
       },
     }
+  end,
+  config = function()
+    local vimtex_group = vim.api.nvim_create_augroup("VimTeXTracking", { clear = true })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VimtexEventCompileSuccess",
+      group = vimtex_group,
+      callback = function()
+        vim.g.vimtex_tracking = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      group = vimtex_group,
+      pattern = "*.tex",
+      callback = function()
+        if vim.fn.mode() == "n" and vim.g.vimtex_tracking then
+          pcall(vim.cmd, "VimtexView")
+        end
+      end,
+    })
   end,
 }
