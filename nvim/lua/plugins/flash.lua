1b67deab4a39054c9a871d82c9ac4ed34330e0de
Jhonatan-calle <Jhonatancallegaleano@gmail.com>
2026-06-15 19:51:14 -0300

    desable flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,13 +1,4 @@
 return {
   "folke/flash.nvim",
-  keys = {
-    {
-      "s",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
-    },
-  },
+  enabled = false,
 }
